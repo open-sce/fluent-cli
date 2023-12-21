b9b85ae838f43a7d4ed007b3fc18e69365bc0a22
Christine <137011200+ctgao@users.noreply.github.com>
2023-12-21 07:27:12 -0500

    Solution for Issue #36 (#52)
    * I may have fixed the issue? Requires more testing but it seems to work so far
    
    * did some simple command-line testing
    
    * testing works, method 2 also works
    
    * fixed some small things
    
    * Found a better solution to the stack trace printing error.
    
    * Fixed some formatting
    
    * Optimized the getRequiredParam function such that NullPointerExceptions will never be thrown.
    
    * Modified the command job get from a map of parameters to a single option
    
    * modified the order of the options for clarity

--- src/main/java/com/ksm/domino/cli/command/job/JobGet.java
+++ src/main/java/com/ksm/domino/cli/command/job/JobGet.java
@@ -1,16 +1,12 @@
 package com.ksm.domino.cli.command.job;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import com.dominodatalab.api.model.DominoJobsInterfaceJob;
 import com.dominodatalab.api.rest.JobsApi;
 import com.ksm.domino.cli.command.AbstractDominoCommand;
-
 import picocli.CommandLine;
 import picocli.CommandLine.ParentCommand;
 
-@CommandLine.Command(name = "get", header = "%n@|green Retrieves a single job by job id.|@")
+@CommandLine.Command(name = "get", header = "%n@|green Retrieves a single job by job id.|@", sortOptions = false)
 public class JobGet extends AbstractDominoCommand {
 
     @ParentCommand
@@ -18,12 +14,11 @@ public class JobGet extends AbstractDominoCommand {
 
     private static final String NAME = "job get";
 
-    @CommandLine.Parameters(description = "@|blue Required parameters:%n jobId=12345%n|@%n", mapFallbackValue = "")
-    private final Map<String, String> parameters = new LinkedHashMap<>(3);
+    @CommandLine.Option(names = {"--jobId"}, description = "Job ID to get%n", required = true, order = -2)
+    private String jobId;
 
     @Override
     public void execute() throws Exception {
-        String jobId = getRequiredParam(parameters, "jobId", NAME);
         JobsApi api = new JobsApi(getApiClient(parent.domino));
         DominoJobsInterfaceJob job = api.getJob(jobId);
         output(job, parent.domino);
