8a7f819f72667f99724f2dd5ef99ff3178243fd6
Kevin J Hoerr <khoerr@ksmpartners.com>
2024-02-26 17:21:34 -0500

    Switch project copy command to use Domino public API

--- src/main/java/com/ksm/domino/cli/command/project/ProjectCopy.java
+++ src/main/java/com/ksm/domino/cli/command/project/ProjectCopy.java
@@ -4,17 +4,16 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 
-import com.dominodatalab.api.model.DominoNucleusProjectModelsForkOrCopyProject;
-import com.dominodatalab.api.model.DominoNucleusProjectModelsForkOrCopyProject.VisibilityEnum;
-import com.dominodatalab.api.model.DominoNucleusProjectModelsProject;
-import com.dominodatalab.api.model.DominoProjectsApiCopiedGitRepoMetadata;
-import com.dominodatalab.api.model.DominoProjectsApiCopyGitRequest;
-import com.dominodatalab.api.model.DominoProjectsApiLinkedGitRepoMetadata;
-import com.dominodatalab.api.model.DominoProjectsApiNewTagsDTO;
-import com.dominodatalab.api.rest.ProjectsApi;
+import com.dominodatalab.pub.model.CopyProjectSpecV1;
+import com.dominodatalab.pub.model.DeepCopyGitRepoSpecV1;
+import com.dominodatalab.pub.model.GitCodeRepoSpecV1;
+import com.dominodatalab.pub.model.ProjectCopyResultEnvelopeV1;
+import com.dominodatalab.pub.model.ProjectVisibilityV1;
+import com.dominodatalab.pub.model.ProviderRepoVisibilityV1;
+import com.dominodatalab.pub.model.ReferenceCopyGitRepoSpecV1;
+import com.dominodatalab.pub.rest.ProjectsApi;
 import com.ksm.domino.cli.command.AbstractDominoCommand;
 
 import picocli.CommandLine;
@@ -29,15 +28,15 @@ public class ProjectCopy extends AbstractDominoCommand {
     private static final String NAME = "project copy";
     private static final String PARAM_COPY_PROJECT_ID = "projectToCopyId";
     private static final String PARAM_NAME = "name";
-    private static final String PARAM_DESCRIPTION = "description";
     private static final String PARAM_OWNER_ID = "ownerId";
     private static final String PARAM_CREDENTIAL_ID = "credentialId";
     private static final String PARAM_IMPORTED_CREDENTIAL_ID = "importedCredentialId";
     private static final String PARAM_LINK_REPO_URL = "linkRepoUrl";
     private static final String PARAM_REPO_NAME = "repoName";
     private static final String PARAM_REPO_OWNER = "repoOwner";
+    private static final String PARAM_COPY_DATASETS = "copyDatasets";
 
-    @CommandLine.Parameters(description = "@|blue Required Parameters:%nname=projectName%nownerId=6125...%nprojectToCopyId=1235...%ncredentialId=6d321...%n%nOptional Parameters:%ndescription=\"A project description\"%nimportedCredentialId=6e124...%nlinkRepoUrl=https://github.com/...%nrepoName=domino-project%nrepoOwner=user|@")
+    @CommandLine.Parameters(description = "@|blue Required Parameters:%nname=projectName%nownerId=6125...%nprojectToCopyId=1235...%ncredentialId=6d321...%n%nOptional Parameters:%nimportedCredentialId=6e124...%nlinkRepoUrl=https://github.com/...%nrepoName=domino-project%nrepoOwner=user%ncopyDatasets|@")
     private final Map<String, String> parameters = new LinkedHashMap<>();
 
     @Override
@@ -47,8 +46,8 @@ public class ProjectCopy extends AbstractDominoCommand {
         String projectName = getRequiredParam(parameters, PARAM_NAME, NAME);
         String ownerId = getRequiredParam(parameters, PARAM_OWNER_ID, NAME);
         String credentialId = getRequiredParam(parameters, PARAM_CREDENTIAL_ID, NAME);
+        Boolean copyDatasets = parameters.containsKey(PARAM_COPY_DATASETS);
 
-        String description = parameters.getOrDefault(PARAM_DESCRIPTION, StringUtils.EMPTY);
         String importedCredentialId = parameters.getOrDefault(PARAM_IMPORTED_CREDENTIAL_ID, credentialId);
 
         // link existing repo
@@ -58,36 +57,39 @@ public class ProjectCopy extends AbstractDominoCommand {
         Optional<String> repoName = Optional.ofNullable(parameters.get(PARAM_REPO_NAME));
         Optional<String> repoOwner = Optional.ofNullable(parameters.get(PARAM_REPO_OWNER));
 
-        DominoProjectsApiCopyGitRequest gitObject = new DominoProjectsApiCopyGitRequest();
-        gitObject.setCredentialId(credentialId);
-        gitObject.setImportedGitReposCredentialId(importedCredentialId);
+        GitCodeRepoSpecV1 repoSpec = new GitCodeRepoSpecV1();
+        repoSpec.setCredentialId(credentialId);
 
         // - enforce link xor new repo
         if (linkRepoUrl.isPresent() && repoName.isEmpty() && repoOwner.isEmpty()) {
-            gitObject.setLinkSpec(new DominoProjectsApiLinkedGitRepoMetadata().uri(linkRepoUrl.get()));
+            ReferenceCopyGitRepoSpecV1 refSpec = new ReferenceCopyGitRepoSpecV1();
+            refSpec.setMainRepoUrl(linkRepoUrl.get());
+
+            repoSpec.setReferenceCopy(refSpec);
         } else if (repoName.isPresent()) {
             Validate.notBlank(repoOwner.orElse(null), "Parameter 'repoOwner' is required if 'repoName' is specified");
-            DominoProjectsApiCopiedGitRepoMetadata copySpec = new DominoProjectsApiCopiedGitRepoMetadata();
-            copySpec.setVisibility(DominoProjectsApiCopiedGitRepoMetadata.VisibilityEnum.PRIVATE);
-            copySpec.setRepoName(repoName.get());
-            copySpec.setOwnerName(repoOwner.get());
-            gitObject.setCopySpec(copySpec);
+            DeepCopyGitRepoSpecV1 copySpec = new DeepCopyGitRepoSpecV1();
+            copySpec.setVisibility(ProviderRepoVisibilityV1.PRIVATE);
+            copySpec.setNewRepoName(repoName.get());
+            copySpec.setNewRepoOwnerName(repoOwner.get());
+
+            repoSpec.setDeepCopy(copySpec);
         } else {
             throw new IllegalArgumentException("Must specify to link an existing git repo or copy from the template project's template repository: use parameter 'linkRepoUrl' or parameters 'repoName' and 'repoOwner'");
         }
 
         // Create base request - assume Private, no tags or collaborators (can be added after)
-        DominoNucleusProjectModelsForkOrCopyProject request = new DominoNucleusProjectModelsForkOrCopyProject();
+        CopyProjectSpecV1 request = new CopyProjectSpecV1();
         request.setName(projectName);
-        request.setDescription(description);
+        request.setCopyDatasets(copyDatasets);
         request.setOwnerId(ownerId);
-        request.setVisibility(VisibilityEnum.PRIVATE);
-        request.setTags(new DominoProjectsApiNewTagsDTO());
-        request.setCopyGitRequest(gitObject);
+        request.setVisibility(ProjectVisibilityV1.PRIVATE);
+        request.setGitCodeRepoSpec(repoSpec);
+        request.setImportedGitReposCredentialId(importedCredentialId);
 
-        ProjectsApi projectsApi = new ProjectsApi(getApiClient(parent.domino));
+        ProjectsApi projectsApi = new ProjectsApi(getPubClient(parent.domino));
 
-        DominoNucleusProjectModelsProject project = projectsApi.copyProject(projectToCopyId, request);
+        ProjectCopyResultEnvelopeV1 project = projectsApi.copyProject(projectToCopyId, request);
         output(project, parent.domino);
 
     }
