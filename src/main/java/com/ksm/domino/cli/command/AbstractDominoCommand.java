193520967a5163da6077322706620c6a60840976
Kevin J Hoerr <khoerr@ksmpartners.com>
2024-02-26 19:59:12 -0500

    Explicitly override ObjectMapper for testing

--- src/main/java/com/ksm/domino/cli/command/AbstractDominoCommand.java
+++ src/main/java/com/ksm/domino/cli/command/AbstractDominoCommand.java
@@ -1,23 +1,30 @@
 package com.ksm.domino.cli.command;
 
-import com.dominodatalab.api.invoker.ApiException;
-import com.dominodatalab.client.TrustAllManager;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.toml.TomlMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.ksm.domino.cli.Domino;
-import org.apache.commons.lang3.StringUtils;
-import picocli.CommandLine.Option;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.time.Duration;
 import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import com.dominodatalab.client.DominoApiClient;
+import com.dominodatalab.client.DominoPublicClient;
+import com.dominodatalab.client.TrustAllManager;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.dataformat.toml.TomlMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.ksm.domino.cli.Domino;
+
+import picocli.CommandLine.Option;
 
 /**
  * Abstract base class that any command that needs to access Domino should extend.
@@ -42,13 +49,32 @@ public abstract class AbstractDominoCommand implements Runnable {
     public void run() {
         try {
             execute();
-        } catch (ApiException ex) {
+        } catch (com.dominodatalab.api.invoker.ApiException|com.dominodatalab.pub.invoker.ApiException ex) {
+            ExceptionUtils.printRootCauseStackTrace(ex);
             throw new RuntimeException(ex.getMessage());
         } catch (IllegalArgumentException ex) {
             throw ex;
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    public static ObjectMapper explicitOM() {
+        JsonMapper mapper = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
+            .build();
+        mapper.setSerializationInclusion(Include.NON_NULL);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+        mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+        mapper.enable(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS);
+        mapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+        JavaTimeModule module = new JavaTimeModule();
+        mapper.registerModule(module);
+        return mapper;
     }
 
     /**
@@ -58,7 +84,7 @@ public abstract class AbstractDominoCommand implements Runnable {
      */
     public com.dominodatalab.api.invoker.ApiClient getApiClient(Domino domino) {
         HttpClient.Builder httpClient = HttpClient.newBuilder().sslContext(TrustAllManager.createSslContext());
-        com.dominodatalab.api.invoker.ApiClient client = new com.dominodatalab.api.invoker.ApiClient();
+        com.dominodatalab.api.invoker.ApiClient client = DominoApiClient.createApiClient();
         client.setHttpClientBuilder(httpClient);
         client.setReadTimeout(Duration.ofSeconds(domino.timeoutSeconds));
         client.updateBaseUri(domino.apiUrl);
@@ -78,10 +104,12 @@ public abstract class AbstractDominoCommand implements Runnable {
      */
     public com.dominodatalab.pub.invoker.ApiClient getPubClient(Domino domino) {
         HttpClient.Builder httpClient = HttpClient.newBuilder().sslContext(TrustAllManager.createSslContext());
-        com.dominodatalab.pub.invoker.ApiClient client = new com.dominodatalab.pub.invoker.ApiClient();
+        com.dominodatalab.pub.invoker.ApiClient client = DominoPublicClient.createApiClient();
         client.setHttpClientBuilder(httpClient);
         client.setReadTimeout(Duration.ofSeconds(domino.timeoutSeconds));
         client.updateBaseUri(domino.apiUrl);
+        //TODO ObjectMapper is overridden, this should ideally be controlled from the domino-java-client
+        client.setObjectMapper(explicitOM());
         client.setRequestInterceptor(builder -> builder.setHeader("X-Domino-Api-Key", domino.apiKey));
 
         return client;
@@ -115,17 +143,9 @@ public abstract class AbstractDominoCommand implements Runnable {
             case JSON:
             default:
                 // default to JSON mapper
-                mapper = new ObjectMapper();
+                mapper = DominoApiClient.createDefaultObjectMapper();
                 break;
         }
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
-        mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
-        mapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
-        mapper.registerModule(new JavaTimeModule());
         String result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(o);
         System.out.println(result);
     }
