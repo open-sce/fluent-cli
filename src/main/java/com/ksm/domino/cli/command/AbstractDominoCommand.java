f820f210a918fa6d50bf270815032fe91fc9b65c
Kevin J Hoerr <khoerr@ksmpartners.com>
2024-02-27 11:55:33 -0500

    Remove override objectmapper

--- src/main/java/com/ksm/domino/cli/command/AbstractDominoCommand.java
+++ src/main/java/com/ksm/domino/cli/command/AbstractDominoCommand.java
@@ -1,7 +1,6 @@
 package com.ksm.domino.cli.command;
 
 import java.net.URI;
-import java.net.http.HttpClient;
 import java.time.Duration;
 import java.util.Map;
 
@@ -10,18 +9,10 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import com.dominodatalab.client.DominoApiClient;
 import com.dominodatalab.client.DominoPublicClient;
-import com.dominodatalab.client.TrustAllManager;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.StreamReadFeature;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.dataformat.toml.TomlMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.ksm.domino.cli.Domino;
 
 import picocli.CommandLine.Option;
@@ -59,33 +50,13 @@ public abstract class AbstractDominoCommand implements Runnable {
         }
     }
 
-    public static ObjectMapper explicitOM() {
-        JsonMapper mapper = JsonMapper.builder()
-            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
-            .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
-            .build();
-        mapper.setSerializationInclusion(Include.NON_NULL);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
-        mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
-        mapper.enable(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS);
-        mapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
-        JavaTimeModule module = new JavaTimeModule();
-        mapper.registerModule(module);
-        return mapper;
-    }
-
     /**
      * Create the API Client for accessing Domino over HTTP.
      *
      * @return the {@link com.dominodatalab.api.invoker.ApiClient}
      */
     public com.dominodatalab.api.invoker.ApiClient getApiClient(Domino domino) {
-        HttpClient.Builder httpClient = HttpClient.newBuilder().sslContext(TrustAllManager.createSslContext());
         com.dominodatalab.api.invoker.ApiClient client = DominoApiClient.createApiClient();
-        client.setHttpClientBuilder(httpClient);
         client.setReadTimeout(Duration.ofSeconds(domino.timeoutSeconds));
         client.updateBaseUri(domino.apiUrl);
         client.setRequestInterceptor(builder -> builder.setHeader("X-Domino-Api-Key", domino.apiKey));
@@ -103,15 +74,10 @@ public abstract class AbstractDominoCommand implements Runnable {
      * @return the {@link com.dominodatalab.pub.invoker.ApiClient}
      */
     public com.dominodatalab.pub.invoker.ApiClient getPubClient(Domino domino) {
-        HttpClient.Builder httpClient = HttpClient.newBuilder().sslContext(TrustAllManager.createSslContext());
         com.dominodatalab.pub.invoker.ApiClient client = DominoPublicClient.createApiClient();
-        client.setHttpClientBuilder(httpClient);
         client.setReadTimeout(Duration.ofSeconds(domino.timeoutSeconds));
         client.updateBaseUri(domino.apiUrl);
-        //TODO ObjectMapper is overridden, this should ideally be controlled from the domino-java-client
-        client.setObjectMapper(explicitOM());
         client.setRequestInterceptor(builder -> builder.setHeader("X-Domino-Api-Key", domino.apiKey));
-
         return client;
     }
 
