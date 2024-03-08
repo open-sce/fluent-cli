159b456fa01bd799034a5783764fb54cdd2ee9f0
melloware <mellowaredev@gmail.com>
2024-03-08 11:10:21 -0500

    Fix #80: Respect Domino workspace environment variables

--- src/main/java/com/ksm/domino/cli/Domino.java
+++ src/main/java/com/ksm/domino/cli/Domino.java
@@ -5,10 +5,6 @@ import static picocli.CommandLine.Option;
 import static picocli.CommandLine.ScopeType;
 import static picocli.CommandLine.Spec;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
-import org.fusesource.jansi.AnsiConsole;
-
 import com.ksm.domino.cli.command.collaborator.Collaborator;
 import com.ksm.domino.cli.command.dataset.Dataset;
 import com.ksm.domino.cli.command.datasource.DataSource;
@@ -16,14 +12,16 @@ import com.ksm.domino.cli.command.goals.Goal;
 import com.ksm.domino.cli.command.job.Job;
 import com.ksm.domino.cli.command.project.Project;
 import com.ksm.domino.cli.command.run.Run;
-import com.ksm.domino.cli.command.user.User;
 import com.ksm.domino.cli.command.server.Server;
+import com.ksm.domino.cli.command.user.User;
 import com.ksm.domino.cli.provider.EnvironmentVariableDefaultProvider;
 import com.ksm.domino.cli.provider.OutputExceptionHandler;
 import com.ksm.domino.cli.provider.OutputFormat;
 import com.ksm.domino.cli.provider.VersionProvider;
-
 import io.quarkus.picocli.runtime.annotations.TopCommand;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine;
 
 @TopCommand
@@ -46,12 +44,22 @@ import picocli.CommandLine;
         })
 public class Domino implements Runnable {
 
+    /**
+     * Either the Domino Workspace built in API key or a user set key checked in order.
+     */
+    private static final String ENV_API_KEY = "DOMINO_USER_API_KEY,DOMINO_API_KEY";
+
+    /**
+     * Either the Domino Workspace built in API URL or a user set URL checked in order.
+     */
+    private static final String ENV_API_URL = "DOMINO_API_HOST,DOMINO_API_URL";
+
     @Option(names = {"-k",
-            "--key"}, description = "Domino API Key.", defaultValue = "DOMINO_API_KEY", scope = ScopeType.INHERIT)
+            "--key"}, description = "Domino API Key.", defaultValue = ENV_API_KEY, scope = ScopeType.INHERIT)
     public String apiKey;
 
     @Option(names = {"-u",
-            "--url"}, description = "Domino API URL.", defaultValue = "DOMINO_API_URL", scope = ScopeType.INHERIT)
+            "--url"}, description = "Domino API URL.", defaultValue = ENV_API_URL, scope = ScopeType.INHERIT)
     public String apiUrl;
 
     @Option(names = {"-t",
@@ -85,12 +93,12 @@ public class Domino implements Runnable {
 
     @Override
     public void run() {
-        if (StringUtils.isBlank(apiKey) || StringUtils.equalsIgnoreCase("DOMINO_API_KEY", apiKey)) {
+        if (StringUtils.isBlank(apiKey) || StringUtils.equalsIgnoreCase(ENV_API_KEY, apiKey)) {
             System.err.println("Domino API Key must be set with -k parameter or DOMINO_API_KEY environment variable!");
             return;
         }
 
-        if (StringUtils.isBlank(apiUrl) || StringUtils.equalsIgnoreCase("DOMINO_API_URL", apiUrl)) {
+        if (StringUtils.isBlank(apiUrl) || StringUtils.equalsIgnoreCase(ENV_API_URL, apiUrl)) {
             System.err.println("Domino API URL must be set with -u parameter or DOMINO_API_URL environment variable!");
             return;
         }
