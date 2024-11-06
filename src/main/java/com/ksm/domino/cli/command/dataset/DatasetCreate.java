b05084f0ca50fe7386e09fb2fb70411261025b57
Kevin J Hoerr <khoerr@ksmpartners.com>
2024-11-06 15:31:08 -0500

    Update dataset create command

--- src/main/java/com/ksm/domino/cli/command/dataset/DatasetCreate.java
+++ src/main/java/com/ksm/domino/cli/command/dataset/DatasetCreate.java
@@ -1,11 +1,8 @@
 package com.ksm.domino.cli.command.dataset;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
+import java.util.function.Consumer;
 
 import com.dominodatalab.api.model.DominoDatasetrwApiDatasetRwGrant;
 import com.dominodatalab.api.model.DominoDatasetrwApiDatasetRwGrant.TargetRoleEnum;
@@ -15,7 +12,7 @@ import com.dominodatalab.api.rest.DatasetRwApi;
 import com.ksm.domino.cli.command.AbstractDominoCommand;
 
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
+import picocli.CommandLine;
 import picocli.CommandLine.ParentCommand;
 
 @Command(name = "create", header = "%n@|green Create dataset for project|@")
@@ -24,18 +21,26 @@ public class DatasetCreate extends AbstractDominoCommand {
     @ParentCommand
     private Dataset parent;
 
-    private static final String NAME = "dataset create";
+    @CommandLine.Option(names = {"--project-id"}, description = "ID of the project%n", required = true, order = -2)
+    private String projectId;
 
-    @Parameters(description = "@|blue Required parameters:%n projectId=123%n name=supplemental%n%nOptional Parameters:%ndescription=\"Extraneous Metadata\"%n|@%n", mapFallbackValue = "")
-    private final Map<String, String> parameters = new LinkedHashMap<>(2);
+    @CommandLine.Option(names = {"--name"}, description = "Name of the dataset%n", required = true, order = -2)
+    private String datasetName;
+
+    @CommandLine.Option(names = {"--description"}, description = "Description of the dataset%n", required = false, order = -2)
+    private String datasetDescription;
+
+    @CommandLine.Option(names = {"--owners"}, description = "Usernames to assign as owners of the new dataset", required = false, order = -2)
+    private List<String> owners;
+    
+    @CommandLine.Option(names = {"--editors"}, description = "Usernames to assign as editors to the new dataset", required = false, order = -2)
+    private List<String> editors;
+    
+    @CommandLine.Option(names = {"--readers"}, description = "Usernames to assign as readers to the new dataset", required = false, order = -2)
+    private List<String> readers;
 
     @Override
     public void execute() throws Exception {
-        String projectId = getRequiredParam(parameters,
-                    DominoDatasetrwApiDatasetRwViewDto.JSON_PROPERTY_PROJECT_ID, NAME);
-        String datasetName = getRequiredParam(parameters, "name", NAME);
-
-        String datasetDescription = parameters.getOrDefault("description", StringUtils.EMPTY);
 
         DominoDatasetrwWebCreateDatasetRequest request = new DominoDatasetrwWebCreateDatasetRequest();
         request.projectId(projectId);
@@ -43,22 +48,28 @@ public class DatasetCreate extends AbstractDominoCommand {
         request.description(datasetDescription);
         ArrayList<DominoDatasetrwApiDatasetRwGrant> grants = new ArrayList<>();
 
-        String collaboratorId = parameters.get("collaboratorIds");
-        List<String> collaboratorIds;
-        if (StringUtils.isNotBlank(collaboratorId)) {
-            collaboratorIds = List.of(StringUtils.split(parameters.get("collaboratorIds"), ','));
-            collaboratorIds.forEach(id -> {
-                DominoDatasetrwApiDatasetRwGrant grant = new DominoDatasetrwApiDatasetRwGrant();
-                grant.setTargetId(id);
-            grant.setTargetRole(TargetRoleEnum.DATASET_RW_EDITOR);
-                grants.add(grant);
-            });
-        }
+        owners.forEach(aggregateGrants(grants, TargetRoleEnum.DATASET_RW_OWNER));
+        editors.forEach(aggregateGrants(grants, TargetRoleEnum.DATASET_RW_EDITOR));
+        readers.forEach(aggregateGrants(grants, TargetRoleEnum.DATASET_RW_READER));
         request.setGrants(grants);
         request.usedForModelMonitoring(Boolean.FALSE);
 
         DatasetRwApi api = new DatasetRwApi(getApiClient(parent.domino));
         DominoDatasetrwApiDatasetRwViewDto result = api.createDataset(request);
         output(result, parent.domino);
+    }
+
+    /**
+     * Creates a Consumer method to add a user to a list of grants as the target role.
+     * 
+     * To be passed via {@link Iterable#forEach forEach}.
+     */
+    private Consumer<String> aggregateGrants(List<DominoDatasetrwApiDatasetRwGrant> grants, TargetRoleEnum targetRole) {
+        return id -> {
+            DominoDatasetrwApiDatasetRwGrant grant = new DominoDatasetrwApiDatasetRwGrant();
+            grant.setTargetId(id);
+            grant.setTargetRole(targetRole);
+            grants.add(grant);
+        };
     }
 }
